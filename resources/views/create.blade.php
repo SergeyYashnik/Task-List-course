599860ae41f0d4a8b25525f5b078a00bb6eaf8b8
SergeyYashnik <fggsshugvgdcj@gmail.com>
2025-09-10 14:43:36 +0500

    Forms and CSRF Protection

---
+++ resources/views/create.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('title', 'Add Task')
+
+@section('content')
+
+    <form action="{{ route('tasks.store') }}" method="POST">
+        @csrf
+        <div>
+            <label for="title">Title</label>
+            <input type="text" name="title" id="title">
+        </div>
+
+        <div>
+            <lavel for="description">Description</lavel>
+            <textarea name="description" id="description" rows="5"></textarea>
+        </div>
+
+        <div>
+            <lavel for="long_description">Description</lavel>
+            <textarea name="long_description" id="long_description" rows="10"></textarea>
+        </div>
+
+        <div>
+            <button type="submit">Add Task</button>
+        </div>
+    </form>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Task;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return redirect()->route('tasks.index');
@@ -13,8 +14,13 @@ Route::get('/tasks', function () {
     ]);
 })->name('tasks.index');
 
+Route::view('/tasks/create', 'create')->name('tasks.create');
+
 Route::get('/tasks/{id}', function ($id) {
     return view('show', ['task' => Task::query()->findOrFail($id)]);
 })->name('tasks.show');
 
+Route::post('tasks', function (Request $request) {
+    dd($request->all());
+})->name('tasks.store');
 
