3889786bbe47974cce807f4a41c87b2a311b359e
SergeyYashnik <fggsshugvgdcj@gmail.com>
2025-09-10 12:21:58 +0500

    Layouts Using Template Inheritance

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+
+<head>
+    <title>Laravel 12 Task List App</title>
+</head>
+
+<body>
+    <h1>@yield('title')</h1>
+    <div>
+        @yield('content')
+    </div>
+</body>
+
+</html>
